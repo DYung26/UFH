c97a7d2c3213ab6ad9f46f82730ce5e56e56b6d1
DYung26 <theadstore1407@gmail.com>
2024-09-13 10:09:20 +0100

    Add `AccountService` for managing user accounts - Implemented `linkAccount` method to associate user accounts with provided data. - Added `getAccountByType` method to retrieve accounts based on type for a specific user. - Integrated with `AccountRepository` for database operations related to account management.

---
+++ backend/src/services/accountService.ts
@@ -0,0 +1,21 @@
+import { ApiKeyAccount, OAuthAccount } from "models/Account";
+import { AccountRepository } from "repositories/accountRepository";
+
+export interface IAccountService{
+    linkAccount(userId: string, accountData: ApiKeyAccount | OAuthAccount): Promise<void>;
+    getAccountByType(userId: string, accountType: string): Promise<ApiKeyAccount | OAuthAccount>;
+}
+
+export class AccountService implements IAccountService {
+    constructor(
+        private readonly accountDBRepo: AccountRepository,
+    ) {}
+
+    async linkAccount(userId: string, accountData: ApiKeyAccount | OAuthAccount): Promise<void> {
+        await this.accountDBRepo.insertAccount(userId, accountData);
+    }
+
+    async getAccountByType(userId: string, accountType: string): Promise<ApiKeyAccount | OAuthAccount> {
+        return await this.accountDBRepo.getAccountByType(userId, accountType) as ApiKeyAccount | OAuthAccount;
+    }
+}
