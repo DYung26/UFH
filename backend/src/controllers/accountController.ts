e533a77083fa529aef77bc8713c2113da4093bd1
DYung26 <theadstore1407@gmail.com>
2024-09-13 10:03:32 +0100

    Implement `AccountController` with account linking functionality - Added `linkAccount` method to link user accounts with provided data. - Integrated `jsonResponse` for standardized response formatting. - Utilized `CustomError` for handling user-related errors. - Ensured that `linkAccount` method checks for user existence and updates account data with a timestamp.

---
+++ backend/src/controllers/accountController.ts
@@ -0,0 +1,23 @@
+import { jsonResponse } from "@utils/Response";
+import { RequestHandler } from "express";
+import { StatusCodes } from "http-status-codes";
+import { AccountService } from "services/accountService";
+import { Request, Response } from "express";
+import { CustomError } from "@utils/customError";
+
+
+export class AccountController {
+    constructor(private readonly accountService: AccountService) {}
+
+    linkAccount: RequestHandler = async (req: Request, res: Response) => {
+        const user = req.user;
+        if (!user) {
+            throw new CustomError('User not found', StatusCodes.NOT_FOUND);
+	}
+	const userId = req.user.id;
+	const accountData = req.body;
+        accountData["linkedAt"] = new Date();
+	await this.accountService.linkAccount(userId, accountData);
+	return jsonResponse(res, StatusCodes.OK, 'Account linked successfully');
+    }
+}
