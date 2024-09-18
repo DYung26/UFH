c1ff16eff6eefbc6ca407c4bb142b803179a51a8
DYung26 <theadstore1407@gmail.com>
2024-09-18 04:17:12 +0100

    Configure `Tailwind CSS` with custom font and scrollbar plugin
    - Set up Tailwind CSS to scan .tsx files in the src directory for class names.
    - Extended theme to include `'Roboto'` font family.
    - Added 'tailwind-scrollbar' plugin for custom scrollbar styling.

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.tsx"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [
+    require('tailwind-scrollbar'),
+  ],
+}
+
