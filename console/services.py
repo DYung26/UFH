14315de769bce9da86b331a4f52bb6718b873349
DYung26 <theadstore1407@gmail.com>
2024-09-23 03:29:24 +0100

    Update `services` for account linking and balance retrieval - Changed `BASE_URL` to point to a new IP address for the server - Updated `link` function to accept dynamic `account_type` for linking various account types - Added support for `binance` in the `get_balance` function - Implemented `get_total_balance` to calculate total balance across all linked accounts - Created `get_linked_accounts` to retrieve a list of linked accounts for the user - Added `get_account_balance` to return balance details for each linked account - Commented out print statements for a cleaner output during requests

--- console/services.py
+++ console/services.py
@@ -1,5 +1,5 @@
 import requests
-BASE_URL = "http://localhost:3000"
+BASE_URL = "http://100.26.225.110:3000" # "http://localhost:3000"
 
 def login(email: str, password: str):
     response =  requests.post(f"{BASE_URL}/api/auth/login", json={"email": email, "password": password })
@@ -7,7 +7,7 @@ def login(email: str, password: str):
         print("Login successful!")
         return response.json()
     else:
-        print("Login failed. Please check your credentials.")
+        print("Login failed. Please check your credentials.\n")
 
 def signup(email: str, password: str, username: str):
     response = requests.post(f"{BASE_URL}/api/auth/register",
@@ -32,17 +32,17 @@ def logout(token: str):
     response = requests.post(f"{BASE_URL}/api/auth/logout", headers=headers)
     if response.status_code == 200:
         print("Logged out...")
-        print(response.json())
+        # print(response.json())
 
-def link(token, key, secret):
+def link(token, key, secret, account_type):
     headers = {
         "Authorization": f"Bearer {token}"
     }
     response = requests.post(f"{BASE_URL}/api/accounts/link",
-                             headers=headers, json={"accountType": "bybit", "apiKey": key,
-                                                    "apiSecret": secret})
+                             headers=headers, json={"account_type": account_type, "api_key": key,
+                                                    "api_secret": secret})
     if response.status_code == 200:
-        print(response.json())
+        # print(response.json())
         return response.json()
 
 def get_balance(token, account_type):
@@ -54,10 +54,41 @@ def get_balance(token, account_type):
         params = {
             "accountType": "bybit",
         }
+    elif account_type == "binance":
+        params = {
+            "accountType": "binance",
+        }
     response = requests.get(f"{BASE_URL}/api/balance/", headers=headers, params=params)
     if response.status_code == 200:
-        print(response.json())
+        # print(response.json())
         return response.json()["data"]["balance"]
     else:
         return 0
+
+def get_total_balance(token):
+    linked_accounts = get_linked_accounts(token)
+    total_balance = 0
+    for account in linked_accounts:
+        account_type = account["account_type"]
+        total_balance += float(get_balance(token, account_type))
+    return total_balance
+
+def get_linked_accounts(token):
+    headers = {
+        "Authorization": f"Bearer {token}"
+    }
+    response = requests.get(f"{BASE_URL}/api/accounts/linked-accounts", headers=headers)
+    if response.status_code == 200:
+        # print(response.json())
+        return response.json()["data"]
+    return []
+
+def get_account_balance(token):
+    linked_accounts = get_linked_accounts(token)
+    account_balance = [
+        [account["account_type"], get_balance(token, account["account_type"])]
+        for account in linked_accounts
+    ]
+    # print(account_balance)
+    return account_balance
 
